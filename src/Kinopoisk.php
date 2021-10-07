47510623391ec0caf1bbc0092995890d62bf3291
coolswitcher <CoolSwitcher@gmail.com>
2021-10-08 01:07:27 +0300

    api v2.2

--- src/Kinopoisk.php
+++ src/Kinopoisk.php
@@ -32,7 +32,7 @@ abstract class Kinopoisk {
      * UserAgent
      * @var string
      */
-    protected $_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A5297c Safari/602.1';
+    protected $_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_4 like Mac OS X) AppleWebKit/692.5.38 (KHTML, like Gecko) Version/12.4 Mobile/14A578c Safari/692.3';
 
     /**
      * Referer
